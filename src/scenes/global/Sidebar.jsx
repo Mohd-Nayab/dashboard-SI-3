b8247af203ddce1943d5ce583a2d5a62ec1dbb94
nayab-si <132437221+nayab-si@users.noreply.github.com>
2023-09-18 12:27:50 +0530

    project created

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -97,7 +97,7 @@ const Sidebar = () => {
                                     alt="profile-user"
                                     width="100px"
                                     height="100px"
-                                    src={`../../assets/user_img`}
+                                    src={`../../assets/user_img.jpeg`}
                                     style={{ cursor: "pointer", borderRadius: "50%" }}
                                 />
                             </Box>
